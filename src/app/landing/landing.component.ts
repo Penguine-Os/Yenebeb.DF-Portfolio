910e709117e7421afd6f1a1d5e21a6fc483dc8f9
Yenebeb Daniel Fikre <r0901225@student.thomasmore.be>
2023-05-17 23:17:56 +0200

    change to sub

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {GithubService} from '../services/github.service';
+import {Repository} from '../../../Models/Repository';
+import {BehaviorSubject, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent implements OnInit {
-
+export class LandingComponent implements OnInit, OnDestroy {
+  public repos: Repository[]= []
+  private sub: Subscription = new Subscription();
   constructor(public githubService: GithubService) { }
 
-  ngOnInit(): void {
+ async ngOnInit(): Promise<void> {
+    this.sub = this.githubService.getRepos().subscribe(x => this.repos = x)
   }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe()
+  }
+
 
 }
